935697f94e89c8c847b756c8d1534535e5eb6d17
Sabahat <161129791+Sabahatsabahat@users.noreply.github.com>
2024-09-22 01:47:53 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-center">Hello World</h1>
+    </div>
+  );
+};
+
+export default HomePage;
